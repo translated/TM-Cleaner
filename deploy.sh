be8fea02b24d2e7a1942bc7e34a346a7d74459cc
Domenico Lupinetti <8008416+Ostico@users.noreply.github.com>
2020-10-30 19:23:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ export VERSION=$(jq -r .version version.json)
 export HASH=$(git rev-parse --short HEAD)
 export REGISTRY_NAMESPACE="translatednet"
 
-DOCKER_BUILDKIT=1 docker build --force-rm --no-cache --tag "$NAME" --network=host .
+# DOCKER_BUILDKIT=1 docker build --force-rm --no-cache --tag "$NAME" --network=host .
 
 IMAGE="$REGISTRY_NAMESPACE/$NAME:$VERSION"
 echo Deploying "$IMAGE" on production
-docker tag "$NAME" "$IMAGE"
-docker push "$IMAGE"
+# docker tag "$NAME" "$IMAGE"
+# docker push "$IMAGE"
 DOCKER_HOST="ssh://root@tesla.translated.com:2222" IMAGE="$IMAGE" docker stack deploy --with-registry-auth -c docker-compose.production.yml "$NAME"
